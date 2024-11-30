9c7ceab58806825897da9e70644c96bdc16e85ad
borossandor27 <boros.sanyi@gmail.com>
2024-11-30 15:50:21 +0100

    6.32

--- 09-golfklub/routes/ugyfel.js
+++ 09-golfklub/routes/ugyfel.js
@@ -4,12 +4,26 @@ import * as db from "../db.js"; // Az adatbázis kapcsolat kódjának betöltés
 
 // Ügyfél létrehozása
 router.post("/register", async (req, res) => {
-  res.send("Új ügyfél létrehozva");
+  let result= await db.createUgyfel(req.body);
+  if (result.success) {
+    res.status(201).json(result.data);
+    console.log(result.message);
+  } else {
+    res.status(500).json(result.error);
+    console.log(result.message);
+  }
 });
 
-// Ügyfél belépése
+// Ügyfél belépése (felhasználónév és jelszó ellenőrzése)
 router.post("/login", async (req, res) => {
-  res.send("Ügyfél belépett");
+  let result = await db.loginUgyfel(req.body);
+  if (result.success) {
+    res.status(201).json(result.data);
+    console.log(result.message);
+  } else {
+    res.status(500).json(result.error);
+    console.log(result.message);
+  }
 });
 
 // Ügyfelek listája
@@ -22,14 +36,36 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Egy ügyfél adatai
+router.get("/:uazon", async (req, res) => {
+  let result = await db.getUgyfel(req.params.uazon);
+  if (result.success) {
+    res.status(201).json(result.data);
+  } else {
+    res.status(500).json(result.error);
+  }
+});
+
 // Ügyfél módosítása
 router.put("/:uazon", async (req, res) => {
-  res.send(`Ügyfél módosítva: ${req.params.uazon}`);
+  let result = await db.updateUgyfel(req.params.uazon, req.body);
+  console.log(result.message);
+  if (result.success) {
+    res.status(201).json(result.data);
+  } else {
+    res.status(500).json(result.error);
+  }
 });
 
 // Ügyfél törlése
 router.delete("/:uazon", async (req, res) => {
-  res.send(`Ügyfél törölve: ${req.params.uazon}`);
+  let result = await db.deleteUgyfel(req.params.uazon);
+  console.log(result.message);
+  if (result.success) {
+    res.status(201).json(result.data);
+  } else {
+    res.status(500).json(result.error);
+  }
 });
 
 export default router;
