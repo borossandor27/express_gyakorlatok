52edd2e471d554c75f0c2c5dd4038e099f41a546
borossandor27 <boros.sanyi@gmail.com>
2025-03-09 21:34:18 +0100

    6.6.1

--- 11-Sequelize/index.js
+++ 11-Sequelize/index.js
@@ -1,57 +1,74 @@
-const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
-const app = express();
-const port = 3000;
+import express from 'express';
+import { Sequelize, DataTypes, Model } from 'sequelize';
 
-// Sequelize kapcsolat
-const sequelize = new Sequelize('pizza', 'root', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
+const sequelize = new Sequelize('pizza', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
 });
 
-// Modellek betöltése
-const Futar = require('./models/futar')(sequelize, DataTypes);
-const Pizza = require('./models/pizza')(sequelize, DataTypes);
-const Vevo = require('./models/vevo')(sequelize, DataTypes);
-const Rendeles = require('./models/rendeles')(sequelize, DataTypes);
-const Tetel = require('./models/tetel')(sequelize, DataTypes);
+class Futar extends Model {}
+Futar.init({
+    fazon: { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
+    fnev: { type: DataTypes.STRING(25), allowNull: false },
+    ftel: { type: DataTypes.STRING(15), allowNull: false }
+}, { sequelize, modelName: 'futar', timestamps: false });
 
-// Kapcsolatok definiálása
+class Pizza extends Model {}
+Pizza.init({
+    pazon: { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
+    pnev: { type: DataTypes.STRING(15), allowNull: false },
+    par: { type: DataTypes.INTEGER, allowNull: false }
+}, { sequelize, modelName: 'pizza', timestamps: false });
+
+class Vevo extends Model {}
+Vevo.init({
+    vazon: { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
+    vnev: { type: DataTypes.STRING(30), allowNull: false },
+    vcim: { type: DataTypes.STRING(30), allowNull: false }
+}, { sequelize, modelName: 'vevo', timestamps: false });
+
+class Rendeles extends Model {}
+Rendeles.init({
+    razon: { type: DataTypes.INTEGER.UNSIGNED, primaryKey: true, autoIncrement: true },
+    vazon: { type: DataTypes.INTEGER.UNSIGNED, references: { model: Vevo, key: 'vazon' } },
+    fazon: { type: DataTypes.INTEGER.UNSIGNED, references: { model: Futar, key: 'fazon' } },
+    idopont: { type: DataTypes.DATE, allowNull: false }
+}, { sequelize, modelName: 'rendeles', timestamps: false });
+
+class Tetel extends Model {}
+Tetel.init({
+    razon: { type: DataTypes.INTEGER.UNSIGNED, references: { model: Rendeles, key: 'razon' } },
+    pazon: { type: DataTypes.INTEGER.UNSIGNED, references: { model: Pizza, key: 'pazon' } },
+    db: { type: DataTypes.INTEGER, allowNull: false }
+}, { sequelize, modelName: 'tetel', timestamps: false });
+
+// Model kapcsolatok
 Rendeles.belongsTo(Vevo, { foreignKey: 'vazon' });
 Rendeles.belongsTo(Futar, { foreignKey: 'fazon' });
 Tetel.belongsTo(Rendeles, { foreignKey: 'razon' });
 Tetel.belongsTo(Pizza, { foreignKey: 'pazon' });
 
-// Middleware
+const app = express();
 app.use(express.json());
 
-// Pizza rendelés végpont (tranzakcióval)
+// Rendelés tranzakció kezeléssel
 app.post('/rendeles', async (req, res) => {
-  const { vazon, fazon, pizzak } = req.body;
-
-  const t = await sequelize.transaction();
-
-  try {
-    // Rendelés létrehozása
-    const rendeles = await Rendeles.create({ vazon, fazon, idopont: new Date() }, { transaction: t });
-
-    // Tételek hozzáadása
-    for (const pizza of pizzak) {
-      await Tetel.create({ razon: rendeles.razon, pazon: pizza.pazon, db: pizza.db }, { transaction: t });
+    const { vazon, fazon, tetelek } = req.body;
+    const t = await sequelize.transaction();
+    try {
+        const ujRendeles = await Rendeles.create({ vazon, fazon, idopont: new Date() }, { transaction: t });
+        for (const tetel of tetelek) {
+            await Tetel.create({ razon: ujRendeles.razon, pazon: tetel.pazon, db: tetel.db }, { transaction: t });
+        }
+        await t.commit();
+        res.status(201).json(ujRendeles);
+    } catch (error) {
+        await t.rollback();
+        res.status(500).json({ error: error.message });
     }
-
-    // Tranzakció commit
-    await t.commit();
-
-    res.status(201).json({ message: 'Rendelés sikeresen felvéve!', rendeles });
-  } catch (error) {
-    // Tranzakció rollback
-    await t.rollback();
-    res.status(500).json({ message: 'Hiba történt a rendelés felvételekor', error });
-  }
 });
 
-// Szerver indítása
-app.listen(port, () => {
-  console.log(`Szerver fut a http://localhost:${port} címen.`);
+app.listen(3000, async () => {
+    await sequelize.sync();
+    console.log('Server running on http://localhost:3000');
 });
